2d82fdc99d585b3061adad6906c22a9e7701fd73
rask24 <rask0x18@gmail.com>
2025-11-10 21:24:42 +0900

    [FIX] Add server prefix to PONG response

--- src/CommandRouter.cpp
+++ src/CommandRouter.cpp
@@ -757,16 +757,16 @@ void CommandRouter::handlePing(User* user, const Command& cmd) {
   log(LOG_LEVEL_INFO, LOG_CATEGORY_COMMAND,
       "PING from " + user->getIp() + " - " + debugMsg);
 
-  // RFC 1459: When receiving "PING <token>", reply with "PONG <token>"
-  // The token should be echoed back without the ':' prefix
+  // RFC 1459: Server responses must have prefix ":server"
+  // Format: ":server PONG server :token"
   std::string response;
   if (cmd.params.empty()) {
-    // No token provided, use server name
-    response = "PONG ft_irc\r\n";
+    // No token provided
+    response = ":ft_irc PONG ft_irc\r\n";
   } else {
-    // Echo back the exact token from PING
+    // Echo back the token from PING
     std::string token = cmd.params[0];
-    response = "PONG " + token + "\r\n";
+    response = ":ft_irc PONG ft_irc :" + token + "\r\n";
   }
 
   log(LOG_LEVEL_INFO, LOG_CATEGORY_COMMAND,
